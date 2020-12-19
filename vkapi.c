12d07f486fe0466ac320f2b87de02d68cfe61115
Cortan122 <Cortan122@gmail.com>
2020-12-19 17:26:40 +0300

    vkapi token parameter

--- vkapi.c
+++ vkapi.c
@@ -1,16 +1,22 @@
 #include "botlib.h"
 
-#include <curl/curl.h>
+#include <string.h>
 
 int main(int argc, char** argv){
   if(argc < 2 || argc % 2 == 1){
-    fprintf(stderr, "usage: ./vkapi endpoint [key value]...\n");
+    fprintf(stderr, "usage: ./vkapi [-T token] endpoint [key value]...\n");
     return 1;
   }
 
   char* endpoint = argv[1];
   char* token = "token.txt"; // todo: option
   int returnCode = 1;
+
+  if(strcmp(endpoint, "-T") == 0){
+    token = argv[2];
+    endpoint = argv[3];
+    argv += 2;
+  }
 
   cJSON* res = NULL;
   char* str = NULL;
